abb801671951661bfb8ed19b5d497a1a1cb964aa
JangHyeonJun <janghyeon8725@naver.com>
2023-03-20 13:32:27 +0900

    feat: 404페이지 스타일 및 기능 추가

--- pages/notfound/404page.js
+++ pages/notfound/404page.js
@@ -5,8 +5,9 @@ export function notFoundRender() {
   <div id="wrap">
     <div class="wrap_container">
       <div class="notfound">
-        <div class="">존재하지 않는 페이지 입니다</div>
-        <div class="">3초후 메인페이지로 이동합니다</div>
+        <div class="title">존재하지 않는 페이지 입니다</div>
+        <div class="info">3초후 메인페이지로 이동합니다</div>
+        <div class="clock"></div>
       </div>
     </div>
   </div>
